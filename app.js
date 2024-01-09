93bcf9bef8685bb700553c496a7794736c94ab50
CK <chitrakoneti@gmail.com>
2024-01-10 10:00:59 +1100

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 import express from 'express'
 import { createHandler } from 'graphql-http/lib/use/express';
 import {schema} from "./schemaDefinition/queries/schema.js";
+import mongoose from 'mongoose'
 const app = express()
-const port = 8000
-app.all('/graphql', createHandler({schema}));
+const port = 8001
+//connect to mongo db
+mongoose.connect('mongodb+srv://admin:admin@cluster0.kimditr.mongodb.net/')
+mongoose.connection.once('open', () => {
+    console.log('conneted to database');
+});
 
+app.all('/graphql', createHandler({schema}));
 app.listen(port, ()=>{
     console.log(`server started on ${port}`)
 })
